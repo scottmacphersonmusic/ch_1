81094301b4b72d200a6d17d6679ed28a0e91f311
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-08-31 18:22:38 -0700

    Update project structure

--- spec/features/my_page_spec.rb
+++ spec/features/my_page_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "local page" do
   it "has a welcome header" do
-    visit "/Users/scottmacpherson/Projects/Code_Fellows/ch_1/lib/my_page.html"
+    visit "/Users/scottmacpherson/Projects/Code_Fellows/ch_1/public/my_page.html"
     page.text.must_include "Welcome to my page!"
   end
 end
